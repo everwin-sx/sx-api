ab97659c50751fe93506a366f664beef45ca5096
d.storti <d.storti@everwin.fr>
2022-02-08 09:55:48 +0100

    David Storti : -update service

---
+++ src/main/java/fr/everwin/open/api/services/equipments/FamilyService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 Everwin (www.everwin.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.everwin.open.api.services.equipments;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import fr.everwin.open.api.ClientApi;
+import fr.everwin.open.api.model.equipments.families.Family;
+import fr.everwin.open.api.model.equipments.families.FamilyList;
+import fr.everwin.open.api.services.core.BasicService;
+
+/**
+ * @author d.storti
+ */
+public class FamilyService extends BasicService<Family, FamilyList> {
+
+    protected static final Logger LOGGER = LogManager.getLogger();
+
+    public FamilyService(ClientApi client) {
+        super(client, "families");
+        setModels(Family.class, FamilyList.class);
+    }
+}
